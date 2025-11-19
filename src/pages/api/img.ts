151e0dfd11cb69156a8dfef15cdd2938f506236f
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-11-19 08:17:14 +0100

    wip: file upload

--- src/pages/api/img.ts
+++ src/pages/api/img.ts
@@ -1,5 +1,17 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 import type { APIRoute } from 'astro';
+
+const execAsync = (cmd: string) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+};
 
 export const prerender = false;
 
@@ -22,61 +34,12 @@ export const POST: APIRoute = async ({ request }) => {
   // output into antoher temp file serve back to user
   // delete temp files
 
-  const inputBuffer = Buffer.from(await file.arrayBuffer());
-
-  // spawn lutgen in streaming mode:
-  // We use --input -     → read from STDIN
-  //     --output /dev/stdout → write binary to STDOUT
-  const child = spawn('lutgen', [
-    'apply',
-    '--palette',
-    'gruvbox-dark-hard',
-    '-', // read stdin
-    '--output',
-    './output.jpg', // write stdout
-  ]);
-
-  // Write the uploaded file into lutgen's stdin
-  child.stdin.write(inputBuffer);
-  child.stdin.end();
-
-  // Collect output from stdout
-  const chunks: Buffer[] = [];
-
-  return await new Promise((resolve) => {
-    child.stdout.on('data', (chunk: Buffer<ArrayBufferLike>) => {
-      console.log(chunk);
-      chunks.push(chunk);
-    });
-
-    child.stderr.on('data', (err) => {
-      console.error('lutgen error:', err);
-    });
-
-    child.on('close', (code) => {
-      console.log('CLOSING CODE:', code);
-      if (code !== 0) {
-        resolve(
-          new Response(
-            JSON.stringify({ error: `lutgen exited with code ${code}` }),
-            { status: 500 },
-          ),
-        );
-        return;
-      }
-
-      const output = Buffer.concat(chunks);
-
-      resolve(
-        new Response(output, {
-          status: 200,
-          headers: {
-            'Content-Type': 'image/png',
-            'Content-Length': output.length.toString(),
-            'Cache-Control': 'no-store',
-          },
-        }),
-      );
-    });
+  return new Response(output, {
+    status: 200,
+    headers: {
+      'Content-Type': 'image/png',
+      'Content-Length': output.length.toString(),
+      'Cache-Control': 'no-store',
+    },
   });
 };
